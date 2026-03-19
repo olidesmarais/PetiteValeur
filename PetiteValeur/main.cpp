f9b761ecfae271e1aebc3e99d64c6eb6674dead9
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2026-03-18 23:09:51 -0400

    Initial Commit

---
+++ PetiteValeur/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PetiteValeur
+//
+//  Created by Olivier Desmarais on 2026-03-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
